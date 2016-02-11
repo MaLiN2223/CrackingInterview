15565d79a93bcc9f5a6906e98afed5f895748491
MaLiN2223 <szyszka45@gmail.com>
2016-02-11 03:26:03 +0100

    6 tasks + tests

---
+++ Chapter1/Stringer.cs
@@ -0,0 +1,318 @@
+﻿#pragma warning disable 1587
+/// <summary>
+/// Chapter 1 | Arrays and Strings
+/// </summary>
+#pragma warning restore 1587
+namespace Chapter1
+{
+    using System.Linq; 
+    using System.Text;
+
+    public static class Stringer
+    {
+
+        #region 1st task
+        /// <summary>
+        /// Implement an algorithm to determine if a string has all unique characters.
+        /// </summary>
+        /// <param name="data">String to determine</param>
+        /// <returns></returns>
+        public static bool Unique(string data)
+        {
+            return UniqueSortBased(data);
+            //return UniqueArrayBased(data);
+            //return UniqueNaive(data);
+        }
+        /// <summary>
+        /// Naive method.
+        /// </summary>
+        /// <remarks>
+        /// Checks every char with rest. O(n) complexity and no additional memory.
+        /// </remarks>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static bool UniqueNaive(string data)
+        {
+            int size = data.Length;
+            for (int i = 0; i < size; ++i)
+            {
+                char c = data[i];
+                for (int j = i + 1; j < size; ++j)
+                {
+                    if (c == data[j])
+                        return false;
+                }
+            }
+            return true;
+        }
+        private static bool[] UniqueArray = new bool[256];
+        /// <summary>
+        /// Method based on bool array.
+        /// </summary>
+        /// <remarks>
+        /// Checks if character existed based on bool 128 long bool array. We asume that there is 256 unique characters. O(n) complexity and O(1) additional memory
+        /// </remarks>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static bool UniqueArrayBased(string data)
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                UniqueArray[i] = false;
+            }
+            int size = data.Length;
+            for (int i = 0; i < size; ++i)
+            {
+                if (UniqueArray[data[i]])
+                    return false;
+                UniqueArray[data[i]] = true;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Method based on sorting the string
+        /// </summary>
+        /// <remarks>
+        /// Checks if character existed by comparing nearby elements in sorted list. O(n*log(n)) complexity ( n (ToList) + n*log(n) (sort) ) O(n) additional memory
+        /// </remarks>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static bool UniqueSortBased(string data)
+        {
+            int size = data.Length;
+            var sorted = data.ToList();
+            sorted.Sort();
+            for (int i = 0; i < size - 1; ++i)
+            {
+                if (sorted[i] == sorted[i + 1])
+                    return false;
+            }
+            return true;
+        }
+        #endregion
+        #region 2nd task
+        /// <summary>
+        /// Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static char[] Reverse(char[] array)
+        {
+            return ReverseInPlace(array);
+            //return ReverseSecondArray(array);
+        }
+        /// <summary>
+        /// Typical reverse by using second array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+
+        private static char[] ReverseSecondArray(char[] array)
+        {
+
+            int k = array.Length;
+            char[] output = new char[k];
+            for (int i = k - 2; i >= 0; --i)
+            {
+                output[k - i - 2] = array[i];
+            }
+            return output;
+        }
+
+        private static char[] ReverseInPlace(char[] array)
+        {
+            int i = 0; // first pointer
+            int k = array.Length - 2;//second pointer (-2 because of \0 char)
+            while (i < k)
+            {
+                char tmp = array[i];
+                array[i] = array[k];
+                array[k] = tmp;
+                --k;
+                ++i;
+            }
+            return array;
+        }
+        #endregion
+        #region 3rd task
+        /// <summary>
+        /// Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer.
+        /// </summary>
+        /// <remarks> 
+        /// O(n^2) complexity ( 1/2 * n * (n+1) ), no additional memory
+        /// </remarks>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static char[] RemoveDuplicates(char[] data)
+        {
+            int size = data.Length;
+            int start = 0;
+            int removedCount = 0;
+            while (start < size - 1 && start < size - removedCount)
+            {
+                var removed = 0;
+                int ptr = start + 1;
+                while (ptr < size && ptr < size - removedCount) // size - globalremoved == first position where '\0' occurs
+                {
+                    if (data[ptr] == data[start])
+                    {
+                        data[ptr] = '\0';
+                        removed++;
+                    }
+                    else if (removed > 0)
+                        data[ptr - removed] = data[ptr];
+                    ++ptr;
+                }
+
+                start++;
+                removedCount += removed;
+            }
+            return data;
+        }
+        #endregion
+        #region 4th task
+
+        /// <summary>
+        /// Write a method to decide if two strings are anagrams or not.
+        /// </summary>
+        public static bool Anagram(string first, string second)
+        {
+            //return AnagramSorted(first, second);
+            return AnagramArrayBased(first, second);
+        }
+        /// <summary>
+        /// Method based on sorting strings.
+        /// </summary> 
+        /// <remarks>
+        /// Complexity O(n) for creating lists, O(n*log(n)) for sorting and O(n) for comparing thus complexity is O(n*log(n)) and additional space is O(n)
+        /// </remarks>
+        private static bool AnagramSorted(string first, string second)
+        {
+            int size = first.Length;
+            if (size != second.Length)
+                return false;
+            var firstS = first.ToList();
+            var secondS = second.ToList();
+            firstS.Sort();
+            secondS.Sort();
+            for (int i = 0; i < size; ++i)
+            {
+                if (firstS[i] != secondS[i])
+                    return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Method based on two arrays. Both for counting characters in string
+        /// </summary>
+        /// <remarks>
+        /// Complexity O(n), Additional Space O(1)
+        /// </remarks> 
+        private static bool AnagramArrayBased(string first, string second)
+        {
+            int size = first.Length;
+            if (size != second.Length)
+                return false;
+            char[] arrayF = new char[256];
+            char[] arrayS = new char[256];
+            for (int i = 0; i < size; ++i)
+            {
+                char a = first[i];
+                char b = second[i];
+                ++arrayF[a];
+                ++arrayS[b];
+            }
+            for (int i = 0; i < 256; ++i)
+            {
+                if (arrayF[i] != arrayS[i])
+                    return false;
+            }
+            return true;
+        }
+
+        #endregion
+        #region 5th task
+        /// <summary>
+        /// Write a method to replace all spaces in a string with ‘%20’
+        /// </summary> 
+        public static char[] SpaceChange(string text)
+        {
+            //return SpaceChangeStringBuilder(text);
+            return SpaceChangeCounting(text);
+        }
+        /// <summary>
+        /// Method based on StringBuilder class.
+        /// </summary>
+        /// <remarks>
+        /// Complexity depends on Append method.
+        /// </remarks>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static char[] SpaceChangeStringBuilder(string text)
+        {
+            int size = text.Length;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < size; ++i)
+            {
+                if (text[i] == ' ')
+                    builder.Append("%20");
+                else
+                    builder.Append(text[i]);
+            }
+            return builder.ToString().ToCharArray();
+
+        }
+        /// <summary>
+        /// Method based on space counting and helper array
+        /// </summary> 
+        /// <remarks>
+        /// Complexity O(n), additional memory O(n)
+        /// </remarks>
+        private static char[] SpaceChangeCounting(string text)
+        {
+            int count = text.Length;
+            int spacesCount = 0;
+            for (int i = 0; i < count; ++i)
+            {
+                if (text[i] == ' ')
+                    spacesCount++;
+            }
+            char[] array = new char[count + 2 * spacesCount];
+            int k = 0;
+            for (int i = 0; i < count; ++i)
+            {
+                if (text[i] == ' ')
+                {
+                    array[k++] = '%';
+                    array[k++] = '2';
+                    array[k++] = '0';
+                }
+                else
+                    array[k++] = text[i];
+            }
+            return array;
+        }
+        #endregion
+        #region 6th task
+        /// <summary>
+        /// Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees in place.
+        /// </summary> 
+        public static int[][] rotateArray(int[][] array, int n)
+        {
+
+            for (int i = 0; i < n / 2; ++i)
+            {
+                for (int k = i; k < n - 1 - i; ++k)
+                {
+                    int tmp = array[k][n - 1 - i];
+                    array[k][n - 1 - i] = array[i][k];
+                    array[i][k] = array[n - 1 - k][i];
+                    array[n - 1 - k][i] = array[n - 1 - i][n - 1 - k];
+                    array[n - i - 1][n - 1 - k] = tmp;
+                }
+            }
+            return array;
+        }
+        #endregion
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Tests
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using Chapter1;
+    using NUnit.Framework;
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void StringerUniquity()
+        {
+            Assert.IsTrue(Stringer.Unique("abcef"));
+            Assert.IsTrue(Stringer.Unique("zxcvbnmasdfghjklqwertyuiop"));
+            Assert.IsTrue(Stringer.Unique("12azsxdc478fvgbh"));
+            Assert.IsFalse(Stringer.Unique("asdfghjklqwertyuiopzxcvbmna789456"));
+            Assert.IsFalse(Stringer.Unique("1324567897"));
+        }
+
+        [Test]
+        [TestCase("abcdef\0")]
+        [TestCase("hfobnufibef\0")]
+        [TestCase("wretvretbre\0")]
+        [TestCase("5437685b4wnu#@!^%\0")]
+        [TestCase("36nb456wv54#@!^%\0")]
+        [TestCase("$#%BV$#5f$#F%$##@!^%\0")]
+        [TestCase("68726/*/#$#DSFDS\0")]
+        public void StringerReverse(string input)
+        {
+            var data = input.ToCharArray();
+            var output = data.Reverse().ToArray();
+            for (int i = 0; i < data.Length - 1; ++i)
+            {
+                output[i] = output[i + 1];
+            }
+            output[data.Length - 1] = '\0';
+            Assert.AreEqual(output, Stringer.Reverse(data));
+        }
+        [Test]
+        [TestCase("abacadaefa", "abcdef")]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("abcdef", "abcdef")]
+        [TestCase("aaaaaaaaabcdef", "abcdef")]
+        [TestCase("aaaaaaaaaaa", "a")]
+        [TestCase("aabbccddeeff", "abcdef")]
+        [TestCase("aabbbbbcccccdddddeeeeefffff", "abcdef")]
+        public void StringerRemoveDuplicates(string input, string output)
+        {
+            var good = Stringer.RemoveDuplicates(input.ToCharArray());
+            for (int i = 0; i < output.Length; ++i)
+            {
+                Assert.AreEqual(output[i], good[i]);
+            }
+        }
+
+        [Test]
+        [TestCase("a", "a", true)]
+        [TestCase("ab", "ba", true)]
+        [TestCase("bqacbqhqcc", "cqbcbqchqa", true)]
+        [TestCase("jjjajjkaaakakkka", "jajajaaajkkkjkka", true)]
+        [TestCase("abcde", "abcdek", false)]
+        [TestCase("abcdea", "abcdek", false)]
+        public void StringerAnagrams(string first, string second, bool decide)
+        {
+            Assert.AreEqual(decide, Stringer.Anagram(first, second));
+        }
+
+        [Test]
+        [TestCase("a", "a")]
+        [TestCase("a ", "a%20")]
+        [TestCase("a  ", "a%20%20")]
+        [TestCase("  ", "%20%20")]
+        [TestCase("a b c ", "a%20b%20c%20")]
+        public void exchangeSpaces(string input, string output)
+        {
+            Assert.AreEqual(output, Stringer.SpaceChange(output));
+        }
+
+        [Test]
+        public void rotateArray()
+        {
+            int n = 4;
+            int[][] arr = new int[n][];
+            arr[0] = new int[] { 1, 2, 3, 4 };
+            arr[1] = new int[] { 5, 6, 7, 8 };
+            arr[2] = new int[] { 9, 10, 11, 12 };
+            arr[3] = new int[] { 13, 14, 15, 16 };
+            int[][] good = new int[n][];
+            good[0] = new int[] { 13, 9, 5, 1 };
+            good[1] = new int[] { 14, 10, 6, 2 };
+            good[2] = new int[] { 15, 11, 7, 3 };
+            good[3] = new int[] { 16, 12, 8, 4 };
+
+            var output = Stringer.rotateArray(arr, n);
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    Assert.AreEqual(good[i][j], output[i][j]);
+                }
+            }
+
+        }
+    }
+}
